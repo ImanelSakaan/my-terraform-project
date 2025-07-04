3c51e034faa4b3483b09f2937ea4312d44f2d529
ImanelSakaan <elsa0105@algonquinlive.com>
2025-07-04 00:17:08 -0400

    Fix: Add required_version and provider constraints

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
@@ -6,3 +17,4 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
+
